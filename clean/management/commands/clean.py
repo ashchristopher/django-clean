a18e2aa0d1247ad91fbe5da9a9ab8a294c3c28e0
Ash Christopher <ash.christopher@gmail.com>
2010-12-18 21:19:39 -0500

    Added a --force flag that cleans the project without user interaction otherwise user interaction is required.

--- clean/management/commands/clean.py
+++ clean/management/commands/clean.py
@@ -1,17 +1,28 @@
 import os
 import re
-from django.core.management.base import NoArgsCommand
+from optparse import make_option    # in the future, use argparse
+from django.core.management.base import BaseCommand
 from django.conf import settings
+
+
 
 default_clean_types = [
     r'^.+\.pyc$',
 ]
 
-class Command(NoArgsCommand):
+
+class Command(BaseCommand):
     requires_model_validation = False
     can_import_settings = True
+    help = "Cleans up files and directories in a Django project which match given regular expressions."
+    
+    
     project_path = getattr(settings, 'PROJECT_PATH', None)
     clean_types = getattr(settings, 'CLEAN_TYPES', default_clean_types)
+    
+    option_list = BaseCommand.option_list  + (
+        make_option('-f', '--force', action="store_true", default=False, dest='force', help='Force the cleanup without user interaction.'),
+    )
     
     def __init__(self, *args, **kwargs):
         super(Command, self).__init__(*args, **kwargs)
@@ -19,7 +30,7 @@ class Command(NoArgsCommand):
             print "Please define PROJECT_PATH in your settings file."
             exit(1)
     
-    def handle_noargs(self, **kwargs):
+    def handle(self, force, **kwargs):
         print "Cleaning..."
         for dirname, dirnames, filenames in os.walk(self.project_path):
             for filename in filenames:
@@ -27,7 +38,16 @@ class Command(NoArgsCommand):
                 full_path = os.path.join(dirname, filename)
                 for pattern in self.clean_types:
                     if re.search(pattern, full_path):
-                        if os.path.isfile(full_path):
-                            os.remove(full_path)
-                        elif os.path.isdir(full_path):
-                            os.rmdir(full_path)
+                        if force:
+                            self.remove_asset(full_path)
+                        else:
+                            confirm_action = raw_input("Do you want to delete '%s'? [y/N]  " % full_path)
+                            if confirm_action == 'y':
+                                self.remove_asset(full_path)    
+
+    def remove_asset(self, asset):
+        if os.path.isfile(asset):
+            os.remove(asset)
+        elif os.path.isdir(asset):
+            os.rmdir(asset)
+        
