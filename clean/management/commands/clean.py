aec8a4a4426abd0cd5703a6e20480968208db474
Ash Christopher <ash.christopher@gmail.com>
2010-12-18 20:40:02 -0500

    Removes files and directories which match the regex.

--- clean/management/commands/clean.py
+++ clean/management/commands/clean.py
@@ -1,11 +1,17 @@
 import os
+import re
 from django.core.management.base import NoArgsCommand
 from django.conf import settings
+
+default_clean_types = [
+    r'^.+\.pyc$',
+]
 
 class Command(NoArgsCommand):
     requires_model_validation = False
     can_import_settings = True
     project_path = getattr(settings, 'PROJECT_PATH', None)
+    clean_types = getattr(settings, 'CLEAN_TYPES', default_clean_types)
     
     def __init__(self, *args, **kwargs):
         super(Command, self).__init__(*args, **kwargs)
@@ -18,5 +24,10 @@ class Command(NoArgsCommand):
         for dirname, dirnames, filenames in os.walk(self.project_path):
             for filename in filenames:
                 # check against a regex to determine whether to delete file or not
-                # print os.path.join(dirname, filename)
-                pass
+                full_path = os.path.join(dirname, filename)
+                for pattern in self.clean_types:
+                    if re.search(pattern, full_path):
+                        if os.path.isfile(full_path):
+                            os.remove(full_path)
+                        elif os.path.isdir(full_path):
+                            os.rmdir(full_path)
