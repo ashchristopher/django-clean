5cda3053268893cd38bb3316842fdfd00a3a8423
Ash Christopher <ash.christopher@gmail.com>
2011-01-02 23:20:43 -0500

    Fixed setup bugs

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
-try: 
-    from setuptools import setup 
-except ImportError: 
-  from distutils.core import setup
+from setuptools import setup, find_packages
 
     
 setup(
-    name = 'django-clean',
+    name='django-clean',
     version=__import__('clean').__version__,
-    description = 'Clean out files in a django app.',
+    description='Clean out files in a django app using regular expressions.',
     long_description=open('README').read(),
-    author = 'Ash Christopher',
-    author_email = 'ash.christopher@gmail.com',
+    author='Ash Christopher',
+    author_email='ash.christopher@gmail.com',
     url='http://github.com/ashchristopher/django-clean',
     download_url='http://github.com/ashchristopher/django-clean/downloads',
     license='BSD',
     include_package_data=True,
     zip_safe=True, # because we're including media that Django needs
+    keywords='django clean temporary cleanup pyc',
+    packages=find_packages(),
     classifiers=[
         'Development Status :: 5 - Production/Stable',
         'Environment :: Web Environment',
